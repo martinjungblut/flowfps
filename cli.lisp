90b5b357f53ee29ff855e8bbd83acb240e2f5235
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2022-04-04 02:56:26 -0300

    Implement preliminary filter application.

--- cli.lisp
+++ cli.lisp
@@ -1,5 +1,7 @@
 (in-package :flowfps)
 
 (defun cli ()
-  (format t "Hello, world!~&")
+  (open-and-apply "input.png" "greyscale.png" (filter-greyscale-average))
+  (open-and-apply "greyscale.png" "darker-30.png" (filter-darker 30))
+  (format t "Done!~&")
   (finish-output nil))
--- lib.lisp
+++ lib.lisp
@@ -1,37 +1,11 @@
 (in-package :flowfps)
-(declaim (optimize (speed 3) (debug 1) (safety 0)))
+(declaim (optimize (speed 3) (debug 1) (safety 1)))
 
-(defun get-png-filepath (i)
-  (format nil "images_output/~d.png" i))
+(defun rgb+ (value factor)
+  (min (+ value factor) 255))
 
-(declaim (ftype (function ((unsigned-byte 8) (unsigned-byte 8) (unsigned-byte 32) (unsigned-byte 32)) (unsigned-byte 8)) channel-calculate-step))
-(declaim (inline channel-calculate-step))
-(defun channel-calculate-step (a b frame framecount)
-  (nth-value 0 (truncate (* frame (/ (abs (- a b)) (+ 1 framecount))))))
-
-(declaim (ftype (function ((unsigned-byte 8) (unsigned-byte 8) (unsigned-byte 32) (unsigned-byte 32)) (unsigned-byte 8)) channel-apply-step))
-(declaim (inline channel-apply-step))
-(defun channel-apply-step (a b frame framecount)
-  (let ((channel-step (channel-calculate-step a b frame framecount)))
-    (if (> a b)
-      (- a channel-step)
-      (+ a channel-step))))
-
-(defmacro pixel-bind (image y x r g b &rest body)
-  `(multiple-value-bind (,r ,g ,b) (pixel ,image ,y ,x)
-     (declare (type (unsigned-byte 8) ,r ,g ,b))
-     ,@body))
-
-(defun pixel-set-intermediate (img-a img-b img-target y x frame framecount)
-  (declare (type 8-bit-rgb-image img-a img-b img-target))
-  (declare (type (unsigned-byte 32) y x frame framecount))
-  (pixel-bind img-a y x red-a green-a blue-a
-    (pixel-bind img-b y x red-b green-b blue-b
-      (let ((red-c (channel-apply-step red-a red-b frame framecount))
-             (green-c (channel-apply-step green-a green-b frame framecount))
-             (blue-c (channel-apply-step blue-a blue-b frame framecount)))
-        (setf (pixel img-target y x)
-          (values red-c green-c blue-c))))))
+(defun rgb- (value factor)
+  (max (- value factor) 0))
 
 (defmacro image-iterate (image y x &rest body)
   `(with-image-bounds (height width) ,image
@@ -39,27 +13,30 @@
        (loop for ,x below width do
          ,@body))))
 
-(defun write-intermediate-png-files (img-a img-b img-counter framecount)
-  (let ((img-target (copy-image img-a)))
-    (iterate:iter (iterate:for frame from 1 to framecount)
-      (image-iterate img-a y x
-        (pixel-set-intermediate img-a img-b img-target y x frame framecount))
-      (write-png-file (get-png-filepath (+ frame img-counter)) img-target))))
+(defmacro pixel-bind (image y x r g b a &rest body)
+  `(multiple-value-bind (,r ,g ,b ,a) (pixel ,image ,y ,x)
+     (declare (type (unsigned-byte 8) ,r ,g ,b ,a))
+     ,@body))
 
-(defun blend-images (img-a-path img-b-path img-counter framecount)
-  (let ((img-a (read-png-file img-a-path))
-         (img-b (read-png-file img-b-path)))
-    (write-png-file (get-png-filepath img-counter) img-a)
-    (write-intermediate-png-files img-a img-b img-counter framecount)))
+(defun apply-single-image-rgba-filter (image callback)
+  (declare (type 8-bit-rgba-image image))
+  (image-iterate image y x
+    (pixel-bind image y x r g b a
+      (setf (pixel image y x) (funcall callback r g b a)))))
 
-(defun blend-directory (dir-path framecount)
-  (let* ((img-counter 1))
-    (loop for pair in (loop for (a b) on (cl-fad:list-directory dir-path) collect (list a b)) do
-      (let* ((img-a-path (nth 0 pair))
-              (img-b-path (nth 1 pair)))
-        (if img-b-path
-          (progn
-            (print (format nil "Blending images ~A + ~A" img-a-path img-b-path))
-            (print (format nil "img-counter=~d" img-counter))
-            (blend-images img-a-path img-b-path img-counter framecount)
-            (setf img-counter (+ img-counter framecount 1))))))))
+(defun open-and-apply (input-image-path output-image-path callback)
+  (let ((image (read-png-file input-image-path)))
+    (apply-single-image-rgba-filter image callback)
+    (write-png-file output-image-path image)))
+
+(defun filter-greyscale-average ()
+  (lambda (r g b a)
+    (multiple-value-bind (n _) (round (/ (+ r g b) 3))
+      (values n n n a))))
+
+(defun filter-darker (factor)
+  (lambda (r g b a)
+    (let* ((new-r (rgb- r factor))
+            (new-g (rgb- g factor))
+            (new-b (rgb- b factor)))
+      (values new-r new-g new-b a))))
