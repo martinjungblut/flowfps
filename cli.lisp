7dbf1ea84696b17460da2d239f6e397d257a0313
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2022-04-04 05:34:39 -0300

    Add some new filters.

--- cli.lisp
+++ cli.lisp
@@ -1,9 +1,9 @@
 (in-package :flowfps)
 
 (defun cli ()
-  (open-and-apply "input.png" "greyscale.png" (filter-greyscale-average))
-  (open-and-apply "greyscale.png" "darker-30.png" (filter-darker 30))
-  (open-and-apply "input.png" "experimental.png" (filter-experimental))
-  (open-and-apply "experimental.png" "experimental-darker.png" (filter-darker 30))
+  (open-and-apply "input.png" "experimental.png" (filter-noise 5 40 5))
+  (open-and-apply "experimental.png" "experimental.png" (filter-rgb-lshift))
+  (open-and-apply "experimental.png" "experimental.png" (filter-rgb-lshift))
+  (open-and-apply "experimental.png" "experimental.png" (filter-decrease-brightness 10))
   (format t "Done!~&")
   (finish-output nil))
--- lib.lisp
+++ lib.lisp
@@ -1,12 +1,6 @@
 (in-package :flowfps)
 (declaim (optimize (speed 3) (debug 1) (safety 1)))
 
-(defun rgb+ (value factor)
-  (min (+ value factor) 255))
-
-(defun rgb- (value factor)
-  (max (- value factor) 0))
-
 (defmacro image-iterate (image y x &rest body)
   `(with-image-bounds (height width) ,image
      (loop for ,y below height do
@@ -29,18 +23,48 @@
     (apply-single-image-rgba-filter image callback)
     (write-png-file output-image-path image)))
 
+(defun rgb+ (value factor)
+  (min (+ value factor) 255))
+
+(defun rgb- (value factor)
+  (max (- value factor) 0))
+
+(defun rgb/ (value factor)
+  (nth-value 0 (round (/ value factor))))
+
 (defun filter-greyscale-average ()
   (lambda (r g b a)
-    (multiple-value-bind (n _) (round (/ (+ r g b) 3))
+    (let ((n (nth-value 0 (round (/ (+ r g b) 3)))))
       (values n n n a))))
 
-(defun filter-darker (factor)
+(defun filter-decrease-brightness (factor)
   (lambda (r g b a)
     (let* ((new-r (rgb- r factor))
             (new-g (rgb- g factor))
             (new-b (rgb- b factor)))
       (values new-r new-g new-b a))))
 
-(defun filter-experimental ()
+(defun filter-increase-brightness (factor)
+  (lambda (r g b a)
+    (let* ((new-r (rgb+ r factor))
+            (new-g (rgb+ g factor))
+            (new-b (rgb+ b factor)))
+      (values new-r new-g new-b a))))
+
+(defun random-noise (value factor)
+  (rgb- (rgb+ value (random factor)) (random factor)))
+
+(defun filter-noise (r-factor g-factor b-factor)
+  (lambda (r g b a)
+    (let* ((new-r (random-noise r r-factor))
+            (new-g (random-noise g g-factor))
+            (new-b (random-noise b b-factor)))
+      (values new-r new-g new-b a))))
+
+(defun filter-rgb-rshift ()
+  (lambda (r g b a)
+    (values b r g a)))
+
+(defun filter-rgb-lshift ()
   (lambda (r g b a)
     (values g b r a)))
